5597f08fe71c5641c6d2c3983d0e2659044c8058
yngv27@gmail.com <Anthro29!>
2023-07-28 15:29:38 -0400

    Quicksand font

---
+++ QuickS.js
@@ -0,0 +1,63 @@
+exports.getDigit = (d) => {
+    return [{
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,61309,4226,29582,59164,16904,48631,42260,44405,8452,57083,35953,40179,25356,31727]),
+     buffer : require("heatshrink").decompress(atob("AB9niNY0AFBh8RkURrQGB8MUgGli4GBqNAgEDkNgg0VDwXxwEPjAGC0MghIaBAAMCjUHiwGCgsWAxsJjwbEhkcFIkGIIRJBuBBCJIXggFBK4M4JIUIiN3kMqDAXyiMXC4QABsx1CABo="))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,61309,40179,25356,27501,44405,38034,31727,21130,8452,48631,16904,35953,0,0]),
+     buffer : require("heatshrink").decompress(atob("ACFbyAFD1cRmAFCgshAwlBi9xAwem4GBAwYABA34GMoNARCw"))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,61309,4226,35953,31727,44405,33808,8452,57083,48631,21130,16904,23275,25356,27501]),
+     buffer : require("heatshrink").decompress(atob("AB+GkMmwAFBh8hzUhjwGB2MsgEHiUwgFhCIWh8EAxAeCuNwEolB+AGEyPAAocOjIMEwUcAocIyIhEnMWEAuTAocGjJACGgWYAoZQBBgnhjOqAANAgHxiIACyCJUA"))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,12678,40179,61309,35953,25356,8452,57083,16904,21130,4226,31727,48631,19049,33808]),
+     buffer : require("heatshrink").decompress(atob("AB2jmYABn4GBqMRAAMYAwMHu93yMaCwcHwNgAwd4j4FDheB+AGDhEUrQGD0IpBw4GC20itEdIIgqByAHE4NQAwmRmAGE0IGFmJGBvoCBh5aC0Md+vRoQGBg2BiNDuAYCgWS2CKPA"))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,12678,61309,35953,33808,31727,44405,57083,48631,8452,29582,40179,4226,16904,21130]),
+     buffer : require("heatshrink").decompress(atob("ACMHqAGEoOAAocLiIGE0eBAwcKwMhAwcHisBAwdR4AGDh0Y6AGD1EagAGDwOwAwcEieZzORnOSgsRAAcfH4YwEAy0I4CHVA="))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,61309,4226,40179,59164,33808,44405,25356,27501,8452,57083,12678,48631,31727,52857]),
+     buffer : require("heatshrink").decompress(atob("AB2kogABpAGBwMRAAMUAwNhxWq1WjAwNx4AbE+IiFl8K5gFChUSqMRrAGBhkRklNiNAgEDo0wgGljIeEhkVEwtRCIIGE0AGEkIFEhMdG4NWAwORjQwBiNduURyBvCkMSvgZDhQtFABY="))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,4226,35953,61309,48631,16904,23275,33808,57083,8452,21130,40179,27501,12678,44405]),
+     buffer : require("heatshrink").decompress(atob("ABUKAokNyNwAoUCqMV0AGClMZsATD4OaAocHjISDgEJjYnEsPGmv3DQdRiMRqQGByMTtVpio1BiOAAYORsEKigYC6MwhQQBEwVQgUZAwkAqPgFoXQBIJLBJAcDinzqJIDxMRipiE8xbEABY="))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,4226,44405,61309,40179,25356,19049,8452,57083,16904,31727,48631,33808,0,0]),
+     buffer : require("heatshrink").decompress(atob("AB3VqoACwECoMRAAVggEoxGIxURwAXDgsVDwkJjAGEsMwAwmR0AFDg8ZBgkMiwGEwOQAwlR6AFDhA8Fho8N4A8FLwIADzKKY"))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,4226,52857,31727,25356,21130,35953,57083,8452,48631,27501,16904,61309,12678,40179]),
+     buffer : require("heatshrink").decompress(atob("AB9q1VSAoUqiMRjGQAwPxnkJ+OAgECjoKBhUWgEMjQQBhcUgEOiYGBggGBgGB8EAlEZAwPtxfFwNAAwMHGAMakAGBoPUssxmQpBHYUHEQMHioQBgUYgEPHQIABiIzEhg7BhPR2EAzAYCqMY0cRuAGBhOhiOlO4mSRKA="))
+   },
+   {
+     width : 14, height : 22, bpp : 4,
+     transparent : -1,
+     palette : new Uint16Array([0,65535,4226,61309,42260,40179,27501,33808,57083,8452,48631,12678,31727,16904,23275,52857]),
+     buffer : require("heatshrink").decompress(atob("ABMiAgcCucRxwGCzET1URoAGBoNCgGfi0AhcYC4UzkEGi4YCoPQg4QBAANx8EGioGC0PghMTkEAhMR8AJB/cC7ER2EAlERiMfFoUA9WKu0UKgkojIGEoJMCAAMHjGQAoUJ0M7BgdTjAuBAAMLiZiBAAfSR6I"))
+   },
+   ][d];
+   };
